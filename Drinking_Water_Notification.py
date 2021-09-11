35df868bec5aad45e7a197d14b31df3afac8ddc7
Deepak Damri <deepakdamri@gmail.com>
2021-09-11 16:37:43 +0530

    initial commit

---
+++ Drinking_Water_Notification.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+# Use pythonw.exe name.py -> To run in background
+notification.notify(
+    title = "Please Drink water",
+    message = "Atleast 4L of water",
+    app_icon = "D:\Coding\Python\Practice\Icon.ico",
+    timeout = 10
+)
